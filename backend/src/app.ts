925391af3c75ec2f87e209045950f1b79dec0dd1
shiri <30csstudent@gmail.com>
2024-04-14 19:03:58 +0500

    added authentication validator

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import morgan from 'morgan'
 import appRoutes from './routes/index.js';
+import cookieParser from 'cookie-parser';
 
 const app=express();
 // app.get('/')
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET))
 // remove in production
 app.use(morgan('dev'))
 
--- backend/src/controllers/user-controller.ts
+++ backend/src/controllers/user-controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction,Request,Response } from "express"
 import UserModels from "../models/User-models.js"
 import { hash,compare } from 'bcrypt'
+import { createToken } from "../utils/token-manager.js"
+import { COOKIE_NAME } from "../utils/constants.js"
 export const getAllUsers=async(req:Request,res:Response,next:NextFunction)=>{
 try {
     const user=await UserModels.find().select('-password')
@@ -20,6 +22,22 @@ export const Signup=async(req:Request,res:Response,next:NextFunction)=>{
         const hashedPassword=await hash(password,10)
         const user= new UserModels({name,email,password:hashedPassword})
         await user.save();
+        res.clearCookie(COOKIE_NAME,{
+            httpOnly:true,
+            path:'/',
+            domain:'localhost',
+            signed:true
+        });
+        const token =createToken(user._id.toString(),user.email,'7d');
+        const expires=new Date()
+        expires.setDate(expires.getDate()+7)
+        res.cookie(COOKIE_NAME,token,{
+            path:'/',
+            domain:'localhost',
+            expires,
+            httpOnly:true,
+            signed:true,
+        });
         return res.status(200).json({message:'ok',id:user._id.toString()})
         
     } catch (error) {
@@ -41,6 +59,22 @@ export const Login=async(req:Request,res:Response,next:NextFunction)=>{
     {
         return res.status(403).send('Incorrect password')
     }
+    res.clearCookie(COOKIE_NAME,{
+        httpOnly:true,
+        path:'/',
+        domain:'localhost',
+        signed:true
+    });
+    const token =createToken(user._id.toString(),user.email,'7d');
+    const expires=new Date()
+    expires.setDate(expires.getDate()+7)
+    res.cookie(COOKIE_NAME,token,{
+        path:'/',
+        domain:'localhost',
+        expires,
+        httpOnly:true,
+        signed:true,
+    });
     return res.status(200).json({message:'ok',id:user._id.toString()});
         
     } catch (error) {
